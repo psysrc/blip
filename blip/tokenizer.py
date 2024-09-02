6687e39a881b9b7501c0b737af23841d73a56dd5
psysrc <sam.copping@yahoo.com>
2024-09-02 23:01:54 +0100

    Basic tokenizer

---
+++ blip/tokenizer.py
@@ -0,0 +1,84 @@
+"""
+Implements the Tokenizer class.
+"""
+
+import logging
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class Token:
+    type: str
+    value: str
+
+    def __str__(self) -> str:
+        return f"{self.type}[{self.value}]"
+
+
+class Tokenizer:
+    """
+    The BLiP Tokenizer.
+
+    Performs lexical analysis of the input DSL to produce a stream of tokens.
+    The tokens can then be parsed by the `Parser` class.
+    """
+
+    def __init__(self, source: str):
+        self.source = source
+        logging.debug("Tokenizer initialised")
+
+        self.current_token = self.__get_next_token_from_stream()
+
+    def next_token(self) -> Token:
+        """
+        Return the next token in the source stream.
+
+        If `end_of_stream() == True`, a sentinel `PROGRAM_END` token is returned.
+
+        If no valid token can be found, a SyntaxError is raised.
+        """
+
+        token_to_return = self.current_token
+
+        self.current_token = self.__get_next_token_from_stream()
+
+        return token_to_return
+
+    def __get_next_token_from_stream(self) -> Token:
+        token_regexes = {
+            r"^\s": None,  # Whitespace (ignore)
+            r"^//.*": None,  # Single-line comments (ignore)
+            r"^INPUT\b": "INPUT",
+            r"^[a-z][a-z0-9_]*\b": "IDENTIFIER",
+            r'^"[^"]*"': "STRING",
+            r"^:=": "EQUALS",
+            r"^\|": "OR",
+        }
+
+        if self.__source_is_empty():
+            return Token(type="PROGRAM_END", value="")
+
+        for regexp, token_type in token_regexes.items():
+            match = re.search(regexp, self.source)
+
+            if match:
+                token_value = match.group()
+                self.source = self.source[len(token_value) :]
+
+                if token_type:
+                    logging.debug("Tokenizer matched token '%s' of type '%s'", token_value, token_type)
+
+                    return Token(type=token_type, value=token_value)
+
+                return self.__get_next_token_from_stream()
+
+        raise SyntaxError(f"Unknown syntax near characters '{self.source[:10]}'")
+
+    def end_of_stream(self) -> bool:
+        """Returns whether the end of the stream has been reached."""
+
+        return self.current_token.type == "PROGRAM_END"
+
+    def __source_is_empty(self) -> bool:
+        return not bool(self.source)
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from blip import hello
+import argparse
+from pathlib import Path
+from blip.tokenizer import Tokenizer
 
 
 def main():
-    hello.hello()
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("-c", "--parse-code")
+
+    args = argument_parser.parse_args()
+    parse_code_path = Path(args.parse_code)
+
+    parse_code = parse_code_path.read_text()
+
+    tokenizer = Tokenizer(parse_code)
+
+    while not tokenizer.end_of_stream():
+        print(f"{tokenizer.next_token()}  ", end="")
+
+    print()
 
 
 if __name__ == "__main__":
