2eb241bc5611bef4946d708c017523848f9614c7
psysrc <sam.copping@yahoo.com>
2025-07-30 19:56:47 +0100

    Interpreter class works with simple 'Hello World' style programs

--- bliplib/interpreter/blip_interpreter.py
+++ bliplib/interpreter/blip_interpreter.py
@@ -6,3 +6,22 @@ Implements the Interpreter class.
 class Interpreter:
     def __init__(self, blip_ir: dict) -> None:
         self.__code = blip_ir
+
+    def run(self, input_strings: list[str]) -> list[str]:
+        match self.__code:
+            case {
+                "type": "program",
+                "statements": [
+                    {
+                        "type": "return",
+                        "expression": {
+                            "type": "literal",
+                            "value": the_string,
+                        },
+                    }
+                ],
+            }:
+                return [the_string]
+
+            case _:
+                raise RuntimeError()
--- test/test_interpreter.py
+++ test/test_interpreter.py
@@ -1,5 +1,22 @@
 from bliplib.interpreter import Interpreter
 
 
-def test_interpreter_creation():
-    Interpreter({})
+def test_interpreter_hello_world():
+    code = {
+        "type": "program",
+        "statements": [
+            {
+                "type": "return",
+                "expression": {
+                    "type": "literal",
+                    "value": "Hello, World!",
+                },
+            }
+        ],
+    }
+
+    input_strings: list[str] = []
+    output_strings: list[str] = Interpreter(code).run(input_strings)
+
+    assert len(output_strings) == 1
+    assert output_strings[0] == "Hello, World!"
