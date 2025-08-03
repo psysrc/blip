cd6c155e3aa09cc5d2cc4dde6493a44d9cf0e7c7
psysrc <sam.copping@yahoo.com>
2025-08-03 17:42:38 +0100

    refactored an interpreter unit test

--- test/test_interpreter.py
+++ test/test_interpreter.py
@@ -3,27 +3,25 @@ from bliplib.interpreter import Interpreter, InterpreterError
 
 
 def test_interpreter_hello_world():
-    code = {
-        "type": "program",
-        "statements": [
-            {
-                "type": "return",
-                "expression": {
-                    "type": "expression",
-                    "value": {
-                        "type": "string_literal",
-                        "value": "Hello, World!",
+    interpreter = Interpreter(
+        {
+            "type": "program",
+            "statements": [
+                {
+                    "type": "return",
+                    "expression": {
+                        "type": "expression",
+                        "value": {
+                            "type": "string_literal",
+                            "value": "Hello, World!",
+                        },
                     },
                 },
-            },
-        ],
-    }
+            ],
+        }
+    )
 
-    input_strings: list[str] = []
-    output_strings: list[str] = Interpreter(code).run(input_strings)
-
-    assert len(output_strings) == 1
-    assert output_strings[0] == "Hello, World!"
+    assert interpreter.run([]) == ["Hello, World!"]
 
 
 def test_unexpected_code_causes_interpreter_error():
