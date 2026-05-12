ee17359b4078c46f87ee6df0fcf6d95568ffcb95
psysrc <sam.copping@yahoo.com>
2026-05-12 23:26:34 +0100

    Using a fixture for the Parser tests

--- test/parser/test_parser.py
+++ test/parser/test_parser.py
@@ -1,60 +1,45 @@
 import pytest
 from bliplib.parser import Parser
 from bliplib.parser.blip_parser import ParserError
-from bliplib.parser.tokenizer import Token, TokenizerError
+from bliplib.parser.tokenizer import TokenizerError
 
 
-def test_invalid_syntax_raises_tokenizer_error():
-    parser = Parser()
+@pytest.fixture
+def parser():
+    return Parser()
 
+
+def test_invalid_syntax_raises_tokenizer_error(parser):
     with pytest.raises(TokenizerError):
         parser.parse("%67£$QGR _++ )(GFds")
 
 
-def test_token_str():
-    token = Token("string_literal", "hello")
-
-    assert str(token) == "TOKEN['string_literal' | 'hello']"
-
-
-def test_bad_expression_raises_parser_error():
-    parser = Parser()
-
+def test_bad_expression_raises_parser_error(parser):
     with pytest.raises(ParserError):
         parser.parse("ret")
 
 
-def test_bad_identifier_statement_raises_parser_error():
-    parser = Parser()
-
+def test_bad_identifier_statement_raises_parser_error(parser):
     with pytest.raises(ParserError):
         parser.parse("hello hello")
 
 
-def test_bad_statement_raises_parser_error():
-    parser = Parser()
-
+def test_bad_statement_raises_parser_error(parser):
     with pytest.raises(ParserError):
         parser.parse(",")
 
 
-def test_bad_decomposition_raises_parser_error():
-    parser = Parser()
-
+def test_bad_decomposition_raises_parser_error(parser):
     with pytest.raises(ParserError):
         parser.parse("foo -> []")
 
 
-def test_bad_index_raises_parser_error():
-    parser = Parser()
-
+def test_bad_index_raises_parser_error(parser):
     with pytest.raises(ParserError):
         parser.parse("foo = bar['z']")
 
 
-def test_string_decomposition_square_brackets():
-    parser = Parser()
-
+def test_string_decomposition_square_brackets(parser):
     assert parser.parse("text -> '[' content ']'") == {
         "type": "program",
         "statements": [
@@ -83,9 +68,7 @@ def test_string_decomposition_square_brackets():
     }
 
 
-def test_string_decomposition_email():
-    parser = Parser()
-
+def test_string_decomposition_email(parser):
     assert parser.parse("email -> name '@' *") == {
         "type": "program",
         "statements": [
@@ -113,9 +96,7 @@ def test_string_decomposition_email():
     }
 
 
-def test_int_variable():
-    parser = Parser()
-
+def test_int_variable(parser):
     assert parser.parse("num = 5") == {
         "type": "program",
         "statements": [
@@ -137,9 +118,7 @@ def test_int_variable():
     }
 
 
-def test_variable_index_with_integer_literal():
-    parser = Parser()
-
+def test_variable_index_with_integer_literal(parser):
     assert parser.parse("ret input[0]") == {
         "type": "program",
         "statements": [
@@ -164,9 +143,7 @@ def test_variable_index_with_integer_literal():
     }
 
 
-def test_variable_index_with_integer_variable():
-    parser = Parser()
-
+def test_variable_index_with_integer_variable(parser):
     assert parser.parse("idx = 0; ret input[idx]") == {
         "type": "program",
         "statements": [
@@ -205,9 +182,7 @@ def test_variable_index_with_integer_variable():
     }
 
 
-def test_empty_list_variable():
-    parser = Parser()
-
+def test_empty_list_variable(parser):
     assert parser.parse("list = []") == {
         "type": "program",
         "statements": [
@@ -229,9 +204,7 @@ def test_empty_list_variable():
     }
 
 
-def test_list_variable_one_element():
-    parser = Parser()
-
+def test_list_variable_one_element(parser):
     assert parser.parse('list = ["a"]') == {
         "type": "program",
         "statements": [
@@ -261,9 +234,7 @@ def test_list_variable_one_element():
     }
 
 
-def test_list_variable_many_elements():
-    parser = Parser()
-
+def test_list_variable_many_elements(parser):
     assert parser.parse('list = ["a", "b", "c"]') == {
         "type": "program",
         "statements": [
