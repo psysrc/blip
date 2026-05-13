3936a8c76443d44766e74b4a8360a841f7733687
psysrc <sam.copping@yahoo.com>
2026-05-13 13:11:12 +0100

    Refactored the reference program unit tests

--- test/unit/program_references/test_reference_programs.py
+++ test/unit/program_references/test_reference_programs.py
@@ -10,7 +10,24 @@ from bliplib.parser import Parser, ParserError
 from bliplib.interpreter import Interpreter, InterpreterError
 
 
-def run_interpreter_test(ref: ReferenceProgram):
+@pytest.mark.parametrize("ref", get_reference_programs())
+def test_reference_program_parsing(ref: ReferenceProgram):
+    """Given a `ReferenceProgram`, test that the Blip code parses into the correct BlipIR."""
+
+    parser = Parser()
+
+    try:
+        actual_blip_ir = parser.parse(ref.blip_code)
+    except ParserError as err:
+        pytest.fail(f"Program reference '{ref.name}': Parser error: {err}")
+
+    assert actual_blip_ir == ref.blip_ir, f"Program reference '{ref.name}': Incorrect Blip IR"
+
+
+@pytest.mark.parametrize("ref", get_reference_programs())
+def test_reference_program_interpreting(ref: ReferenceProgram):
+    """Given a `ReferenceProgram`, test that the BlipIR is interpreted correctly."""
+
     interpreter = Interpreter(ref.blip_ir)
 
     for i, execution in enumerate(ref.executions):
@@ -29,22 +46,3 @@ def run_interpreter_test(ref: ReferenceProgram):
                 pass
             else:
                 pytest.fail(f"Program reference '{ref.name}': Execution #{i + 1}: Did not throw error, output was {output}")
-
-
-def run_parser_test(ref: ReferenceProgram):
-    parser = Parser()
-
-    try:
-        actual_blip_ir = parser.parse(ref.blip_code)
-    except ParserError as err:
-        pytest.fail(f"Program reference '{ref.name}': Parser error: {err}")
-
-    assert actual_blip_ir == ref.blip_ir, f"Program reference '{ref.name}': Incorrect Blip IR"
-
-
-@pytest.mark.parametrize("ref", get_reference_programs())
-def test_reference_program(ref: ReferenceProgram):
-    """Given a `ReferenceProgram`, run all unit tests."""
-
-    run_parser_test(ref)
-    run_interpreter_test(ref)
