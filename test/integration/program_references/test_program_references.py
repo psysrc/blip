582bab45a9f9b665124cd9567a38d4fbdda22599
psysrc <sam.copping@yahoo.com>
2026-05-13 12:32:14 +0100

    Added integration test file and updated reference program unit/integration test module docstrings

--- test/integration/program_references/test_program_references.py
+++ test/integration/program_references/test_program_references.py
@@ -1,6 +1,6 @@
 """
-These are the unit tests for the reference programs in the `docs/refs/` directory.
-This ensures the Blip code can be parsed and produces the correct outputs when interpreted.
+These are the integration tests for the reference programs in the `docs/refs/` directory.
+This ensures the Blip code can be parsed, interpreted, and transpiled into all supported languages.
 """
 
 import pytest
@@ -53,8 +53,8 @@ def test_reference_program(ref: ReferenceProgram):
 
 def test_number_of_reference_programs():
     """
-    Test that the correct number of reference programs were executed
-    This is a belts-and-braces test to make sure the test suite is functioning correctly
+    Check that the correct number of reference programs were tested.
+    This is a belts-and-braces test to make sure the test suite is functioning correctly.
     """
 
     expected_programs = 14
@@ -64,6 +64,6 @@ def test_number_of_reference_programs():
     if actual_programs != expected_programs:
         warnings.warn(
             f"Expected to run {expected_programs} reference program tests but actually ran {actual_programs}"
-            "\nIf you've added/removed reference programs, you probably just need to update the number of expected programs."
+            "\nIf you've added or removed reference programs, you probably just need to update the number of expected programs."
             "\nHowever, if this warning is unexpected, you should check that the test suite is functioning correctly!"
         )
