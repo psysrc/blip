9a60369d0f28d1f8ac6c4bd11ea7d6c1e806c297
psysrc <sam.copping@yahoo.com>
2026-05-13 13:03:40 +0100

    Moved parser and interpreter tests back into unit test directory
    The integration tests won't use these runners, they'll use the Blip program itself rather than the parser and interpreter modules

--- test/integration/program_references/test_reference_programs.py
+++ test/integration/program_references/test_reference_programs.py
@@ -6,15 +6,16 @@ This ensures the Blip code can be parsed, interpreted, and transpiled into all s
 import pytest
 from test.common.ref_progs.classes import ReferenceProgram
 from test.common.ref_progs.getter import get_reference_programs
-from test.common.ref_progs.tests import run_parser_test, run_interpreter_test
 
 
 @pytest.mark.parametrize("ref", get_reference_programs())
 def test_reference_program(ref: ReferenceProgram):
     """Given a `ReferenceProgram`, run all integration tests."""
 
-    run_parser_test(ref)
-    run_interpreter_test(ref)
+    pytest.skip()
+
+    # run_parser_test(ref)  # TODO
+    # run_interpreter_test(ref)  # TODO
     # run_python_test(ref)  # TODO
     # run_cpp_test(ref)  # TODO
     # run_c_test(ref)  # TODO
--- test/unit/program_references/test_reference_programs.py
+++ test/unit/program_references/test_reference_programs.py
@@ -6,7 +6,40 @@ This ensures the Blip code can be parsed and produces the correct outputs when i
 import pytest
 from test.common.ref_progs.classes import ReferenceProgram
 from test.common.ref_progs.getter import get_reference_programs
-from test.common.ref_progs.tests import run_parser_test, run_interpreter_test
+from bliplib.parser import Parser, ParserError
+from bliplib.interpreter import Interpreter, InterpreterError
+
+
+def run_interpreter_test(ref: ReferenceProgram):
+    interpreter = Interpreter(ref.blip_ir)
+
+    for i, execution in enumerate(ref.executions):
+        if execution.expect_success:
+            try:
+                output = interpreter.run(execution.input_strings)
+            except InterpreterError as err:
+                pytest.fail(f"Program reference '{ref.name}': Execution #{i + 1}: {err}")
+            else:
+                assert output == execution.output_strings, f"Program reference '{ref.name}': Execution #{i + 1}: Incorrect program output"
+
+        else:
+            try:
+                output = interpreter.run(execution.input_strings)
+            except InterpreterError:
+                pass
+            else:
+                pytest.fail(f"Program reference '{ref.name}': Execution #{i + 1}: Did not throw error, output was {output}")
+
+
+def run_parser_test(ref: ReferenceProgram):
+    parser = Parser()
+
+    try:
+        actual_blip_ir = parser.parse(ref.blip_code)
+    except ParserError as err:
+        pytest.fail(f"Program reference '{ref.name}': Parser error: {err}")
+
+    assert actual_blip_ir == ref.blip_ir, f"Program reference '{ref.name}': Incorrect Blip IR"
 
 
 @pytest.mark.parametrize("ref", get_reference_programs())
